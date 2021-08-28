969e5133dcab2b97a32f3d45e2cfb2c062a65589
CircleLin <wayne7428@gmail.com>
2021-08-28 23:48:59 +0800

    加入懸浮社群logo

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,84 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+let swiper = new Swiper(".mySwiper", { 
+  
+  grid: {
+    rows: 3,
+    fill: 'row',
+  },
+breakpoints:{
+  768: {
+    slidesPerView: 2,
+    spaceBetween: 0,
+    grid: {
+      rows: 2,
+      fill: 'row',
+    },
+  },
+  992: {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    grid: {
+      rows:2,
+      fill: 'column',
+    },
+  }
+}
+ });   
+
+ let courseSwiper = new Swiper(".js-course-swiper",{
+    slidesPerView: 1,
+    spaceBetween: 20,
+ })
+ 
+ let teachSwiper = new Swiper('.js-teacher-Swiper',{
+   slidesPerView:1,
+   spaceBetween: 30,
+   breakpoints: {
+    768 :{
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    }
+  }
+ })
+
+let levelSwiper = new Swiper(".js-course-level", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+
+  breakpoints: {
+    768 :{
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    }
+  }
 });
+
+let recommendSwiper = new Swiper(".js-recommend",{
+  slidesPerView: 1,
+  spaceBetween: -30,
+  breakpoints: {
+    768 :{
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: -30,
+    }
+  }
+});
+
+
+const elem = document.querySelector('input[name="datepicker"]');
+const datepicker = new Datepicker(elem, {
+buttonClass: 'btn primary',
+nextArrow: '>',
+prevArrow: '<'
+}); 
